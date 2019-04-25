a7afe546e27e08ac63e8538fcb6b9023d00e8291
Sankalp Mittal <36561982+sankalpmittal1911-BitSian@users.noreply.github.com>
2019-04-26 00:23:29 +0530

    Update Problem 1.cpp

--- Daily Problems/Problem 1.cpp
+++ Daily Problems/Problem 1.cpp
@@ -1,1 +1,48 @@
+/*This problem was recently asked by Google.
+
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?*/
+
+#include <bits/stdc++.h> 
+  
+using namespace std; 
+  
+int main() 
+{ 
+    int n;
+    int k;
+    cin >> n;
+    cin>>k;
+    int list[n];
+    bool flag = false;
+    for(int i=0;i<n;++i)
+      cin>>list[i];
+  
+    unordered_set <int> S;
+    int n = 16; 
+    
+    for(int i=0;i<n;++i)
+    {
+      int temp = k - list[i];
+      
+      if(S.find(temp)!=S.end())
+      {
+        flag = true;
+        break;
+      }
+      
+      S.insert(list[i]);
+    } 
+  
+  if(flag == true)
+    cout<<"True"<<endl;
+  else
+    cout<<"False"<<endl;
+  
+  return 0;
+}
+
 
