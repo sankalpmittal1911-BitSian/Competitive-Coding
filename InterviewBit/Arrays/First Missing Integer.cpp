514c1dacfda3e45c4c0ec1b9599533aa292ac1d7
Sankalp Mittal <36561982+sankalpmittal1911-BitSian@users.noreply.github.com>
2019-05-03 14:34:47 +0530

    Create First Missing Integer.cpp

---
+++ InterviewBit/Arrays/First Missing Integer.cpp
@@ -0,0 +1,40 @@
+/*Given an unsorted integer array, find the first missing positive integer.
+
+Example:
+
+Given [1,2,0] return 3,
+
+[3,4,-1,1] return 2,
+
+[-8, -7, -6] returns 1
+
+Your algorithm should run in O(n) time and use constant space.*/
+
+void sep(vector<int> &A)
+{
+    int i=0,j=0;
+    for(i=0;i<A.size();i++)
+    {
+        if(A[i]<=0)
+        {
+            swap(A[i],A[j]);
+            j++;
+        }
+    }
+    A.erase (A.begin(),A.begin()+j);
+}
+int Solution::firstMissingPositive(vector<int> &A) {
+    sep(A);
+    int i;
+    for(i=0;i<A.size();i++)
+    {
+        if(abs(A[i])-1<A.size() && A[abs(A[i])-1]>0)
+            A[abs(A[i])-1]=-A[abs(A[i])-1];
+    }
+    for(i=0;i<A.size();i++)
+    {
+        if(A[i]>0)
+            return i+1;
+    }
+    return A.size()+1;
+}
